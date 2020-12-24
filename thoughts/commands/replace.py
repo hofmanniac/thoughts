443ffd1e25161fab27a2f976bf1c91ae2ec73719
hofmanniac <jeremy.hofmann@outlook.com>
2020-12-24 09:32:55 -0600

    Fixed replace to process token by token.

--- thoughts/commands/replace.py
+++ thoughts/commands/replace.py
@@ -1,17 +1,22 @@
 from thoughts import context as ctx
+# import re
 
 def process(command, context):
     
     target = command["#replace"]
     withset = command["with"]
 
-    new_val = " " + target + " "
+    tokens = str.split(target)
 
-    for key in withset:
-        if key == "#unification": continue
-        if key == "#item": continue
-        
-        compare_token = " " + key + " "
-        new_val = str.replace(new_val, compare_token, " " + withset[key] + " " )
+    new_val = ""
+
+    for token in tokens:
+        if token in withset:
+            new_val = new_val + withset[token]
+        else:
+            new_val = token
+        new_val = new_val + " "
+    
+    new_val = str.strip(new_val)
 
     return new_val
